0f05dccc1e9d1c307d7503d857b4f9882da3ff8b
terokulju95-boop <terokulju95@gmail.com>
2026-08-25 18:48:03 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'darts-v23';
+const CACHE = 'darts-v24';
 
 // App shell. The Firebase SDK and the web fonts live on other origins, so they
 // are cached opportunistically on first successful load (see below) rather than
